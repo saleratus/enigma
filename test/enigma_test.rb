a4381680ab12bf99e03c593ff4ef5e4a3e5f34c1
William Petty <william@snakeweed.com>
2018-11-02 16:59:42 -0600

    Class EnigmaTest: refactor for MessageBundle Integration

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,20 +1,23 @@
 require_relative './test_helper'
 require './lib/enigma'
+require './lib/message_bundle'
 
 class EnigmaTest < Minitest::Test
 
   def setup
     @e = Enigma.new
-    @my_message = "this is so secret ..end.."
+    @message = "hello world end"
+    @key = "02715"
+    @date = "040895"
+    @e.encrypt(@message, @key, @date)
   end
 
   def test_it_exists
     assert_instance_of Enigma, @e
   end
 
-  def test_it_saves_message
-    expected = @my_message
-    assert_equal expected, @e.message
+  def test_it_creates_message_bundle
+    assert_instance_of MessageBundle, @e.m
   end
 
   def test_it_encrypts_message_with_key_and_date
@@ -25,7 +28,7 @@ class EnigmaTest < Minitest::Test
 
   def test_it_encrypts_message_without_key_and_date
     skip
-    output = @e.encrypt(@my_message)
+    output = @e.encrypt(@message)
   end
 
 end
