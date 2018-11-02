b52afe3ff39198e6cbf659462601927b8479a40f
William Petty <william@snakeweed.com>
2018-11-02 16:11:56 -0600

    Class Shift: add shift value calculation

--- lib/shift.rb
+++ lib/shift.rb
@@ -2,12 +2,11 @@ class Shift
   attr_reader :shift_value
 
   def initialize(message_bundle, position)
-    @shift_value = 73
+    @shift_value = calculate_shift(message_bundle.key, message_bundle.date, position)
   end
 
   def calculate_shift(key, date, position)
-
-
+    position_key(key, position) + position_offset(date, position)
   end
 
   def position_key(key, position)
@@ -15,7 +14,8 @@ class Shift
   end
 
   def position_offset(date, position)
-    1
+    date_squared_string = ((date.to_i)**2).to_s
+    single_digit_offset = date_squared_string[(position - 4)].to_i
   end
 
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -6,14 +6,18 @@ class ShiftTest < Minitest::Test
 
   def setup
     @key = "02715"
-    @date = "04895"
+    @date = "040895"
     @m = MessageBundle.new("hello world end", @key, @date)
-    @position = 2 #of possible 4 positions from 0 to 3
+    @position = 2 #of 4 possible positions from 0 to 3
     @s = Shift.new(@m, @position)
   end
 
   def test_it_exists
     assert_instance_of Shift, @s
+  end
+
+  def test_it_has_shift_value
+    assert_instance_of Integer, @s.shift_value
   end
 
   def test_it_calculates_position_key
@@ -25,10 +29,12 @@ class ShiftTest < Minitest::Test
 
   def test_it_calculates_position_offset
     assert_equal 1, @s.position_offset(@date, 0)
+    assert_equal 0, @s.position_offset(@date, 1)
+    assert_equal 2, @s.position_offset(@date, 2)
+    assert_equal 5, @s.position_offset(@date, 3)
   end
 
   def test_it_calculates_shift
-    skip
     @s.calculate_shift(@key, @date, @position)
     assert_equal 73, @s.shift_value
   end
