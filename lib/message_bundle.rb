0c4658f0f20b90eec674c604f08d79395d85b56c
William Petty <william@snakeweed.com>
2018-11-02 10:41:29 -0600

    Refactored most of Class Enigma into Class MessageBundle

--- lib/message_bundle.rb
+++ lib/message_bundle.rb
@@ -1,16 +1,20 @@
 require 'date'
 
-class InputBundle
+class MessageBundle
   attr_reader :message, :key, :date
 
   def initialize(message, key = nil, date = nil)
     @message = message
     @key = key ? key : random_key
-    @date = date ? date : Date.new
+    @date = date ? date : todays_date_string
   end
 
   def random_key
-    rand(100_000)
+    format('%05d', rand(100_000))
+  end
+
+  def todays_date_string
+    Date.new.strftime("%d%m%y")
   end
 
 end
---
+++ test/message_bundle_test.rb
@@ -0,0 +1,62 @@
+require_relative './test_helper'
+require './lib/message_bundle'
+
+class MessageBundleTest < Minitest::Test
+
+  def setup
+    @my_message = "hello world end"
+    @i = MessageBundle.new(@my_message, "02715", "040895")
+  end
+
+  def test_it_exists
+    assert_instance_of MessageBundle, @i
+  end
+
+  def test_it_saves_message
+    expected = @my_message
+    assert_equal expected, @i.message
+  end
+
+  def test_it_saves_given_key
+    assert_equal "02715", @i.key
+  end
+
+  def test_it_saves_given_date
+    date = "040895"
+    @i = MessageBundle.new(@my_message, "77777", date)
+    assert_equal date, @i.date
+  end
+
+  def test_it_creates_random_key
+    100_000.times do
+      key = @i.random_key
+      #print "#{key} "
+      assert_equal 5, key.length
+      key = key.to_i
+      in_range = key >= 0 && key <= 99_999
+      assert in_range
+    end
+  end
+
+  def test_it_assigns_default_random_key
+    @i = MessageBundle.new(@my_message)
+    assert_instance_of String, @i.key
+    assert_equal 5, @i.key.length
+    assert_instance_of Integer, @i.key.to_i
+  end
+
+  def test_it_creates_todays_date_string
+    today = todays_date_string
+    assert_instance_of String, today
+    assert_equal 6, today.length
+    assert_instance_of Integer, today.to_i
+  end
+
+  def test_it_assigns_default_date
+    @i = MessageBundle.new(@my_message, 44444)
+    assert_instance_of String, @i.date
+    assert_equal 6, @i.date.length
+    puts @i.date
+  end
+
+end
