6248475867763a8cbe031139508cfdb4829c980a
William Petty <william@snakeweed.com>
2018-11-02 11:37:12 -0600

    Class ShiftTest: refactor to include setup

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -1,11 +1,19 @@
 require_relative './test_helper'
 require './lib/shift'
+require './lib/message_bundle'
 
 class ShiftTest < Minitest::Test
 
+  def setup
+    @key = "02715"
+    @date = "04895"
+    @i = MessageBundle.new("hello goodbye end", @key, @date)
+    position = 2 #of possible 4 positions from 0 to 3
+    @s = Shift.new(@i, position)
+  end
+
   def test_it_exists
-    actual = Shift.new
-    assert_instance_of Shift, actual
+    assert_instance_of Shift, @s
   end
 
 end
