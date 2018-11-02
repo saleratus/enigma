579974bb6d6ac513703b5701d1a593cff58a6ebc
William Petty <william@snakeweed.com>
2018-11-02 11:20:31 -0600

    Class ShiftTest: test for existence

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -2,4 +2,10 @@ require_relative './test_helper'
 require './lib/shift'
 
 class ShiftTest < Minitest::Test
+
+  def test_it_exists
+    actual = Shift.new
+    assert_instance_of Shift, actual
+  end
+
 end
