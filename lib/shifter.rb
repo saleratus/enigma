ac0b6660e5df9259223051374be989b00b32b4aa
William Petty <william@snakeweed.com>
2018-11-02 21:09:25 -0600

    Class ShifterTest & Shifter: refactor to simplify argument passing

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -4,23 +4,24 @@ class Shifter
   attr_reader :shifts
 
   def initialize(message_bundle)
-    a_shift = calculate_shift(message_bundle.key, message_bundle.date, 0)
-    b_shift = calculate_shift(message_bundle.key, message_bundle.date, 1)
-    c_shift = calculate_shift(message_bundle.key, message_bundle.date, 2)
-    d_shift = calculate_shift(message_bundle.key, message_bundle.date, 3)
-    @shifts = { A: a_shift, B: b_shift, C: c_shift, D: d_shift }
+    @key = message_bundle.key
+    @date = message_bundle.date
+    @shifts = []
+    4.times do |position|
+      @shifts[position] = calculate_shift(position)
+    end
   end
 
-  def calculate_shift(key, date, position)
-    position_key(key, position) + position_offset(date, position)
+  def calculate_shift(position)
+    position_key(position) + position_offset(position)
   end
 
-  def position_key(key, position)
-    key[ position..(position + 1) ].to_i
+  def position_key(position)
+    @key[position..(position + 1)].to_i
   end
 
-  def position_offset(date, position)
-    date_squared_string = ((date.to_i)**2).to_s
+  def position_offset(position)
+    date_squared_string = ((@date.to_i)**2).to_s
     date_squared_string[(position - 4)].to_i
   end
 
