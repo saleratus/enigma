bfaae3f83d4a43e9f6d715828ac0abe0aab7505b
William Petty <william@snakeweed.com>
2018-11-03 19:18:27 -0600

    Class Crack: test shifter is properly initialized

--- test/crack_test.rb
+++ test/crack_test.rb
@@ -19,9 +19,13 @@ class CrackTest < Minitest::Test
   def test_it_holds_instance_variables
     assert_instance_of MessageBundle, @c.m
     assert_instance_of Shifter, @c.shifter
-    assert_equal -1, @c.direction
+    assert_equal (-1), @c.direction
   end
 
-
+  def test_shifter_is_properly_initialized
+    expected_shifts = [1, 0, 2, 5]
+    assert_equal expected_shifts, @c.shifter.shifts
+    assert_equal '00000', @c.shifter.key
+  end
 
 end
